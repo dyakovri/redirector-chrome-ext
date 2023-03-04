6cfd6c65dcd469c2f4b1c1b9d266fd6b327dc041
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-03-04 21:52:58 +0300

    Copy to clipboard fix

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,19 @@
+const unsecuredCopyToClipboard = (text) => { const textArea = document.createElement("textarea"); textArea.value=text; document.body.appendChild(textArea); textArea.focus();textArea.select(); try{document.execCommand('copy')}catch(err){console.error('Unable to copy to clipboard',err)}document.body.removeChild(textArea)};
+
+/**
+ * Copies the text passed as param to the system clipboard
+ * Check if using HTTPS and navigator.clipboard is available
+ * Then uses standard clipboard API, otherwise uses fallback
+*/
+const copyToClipboard = (content) => {
+  if (window.isSecureContext && navigator.clipboard) {
+    navigator.clipboard.writeText(content);
+  } else {
+    unsecuredCopyToClipboard(content);
+  }
+};
+
+
 function makeid(length) {
     var result = '';
     var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -15,7 +31,7 @@ function success(uri) {
     link_element = document.getElementById('link')
     link_element.text = uri
     link_element.href = uri
-    navigator.clipboard.writeText(uri)
+    unsecuredCopyToClipboard(uri)
 }
 
 function fail() {
